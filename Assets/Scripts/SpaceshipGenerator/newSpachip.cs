53c77c34e769490c198a885c99e1038a4c40cf23
mamanelad <elad.maman@mail.huji.ac.il>
2022-03-10 10:08:35 +0200

    2 plates before connecting

--- Assets/Scripts/SpaceshipGenerator/newSpachip.cs
+++ Assets/Scripts/SpaceshipGenerator/newSpachip.cs
@@ -82,20 +82,20 @@ namespace SpaceshipGenerator
             var temp214Vector2 = temp0Vector2.RotateInDegrees(-140);
 
 
-            Vector3 D1 = new Vector3(temp1Vector2.x, temp1Vector2.y, 0);
-            Vector3 D2 = new Vector3(temp2Vector2.x, temp2Vector2.y, 0);
-            Vector3 D3 = new Vector3(temp3Vector2.x, temp3Vector2.y, 0);
-            Vector3 D4 = new Vector3(temp4Vector2.x, temp4Vector2.y, 0);
-            Vector3 D5 = new Vector3(temp5Vector2.x, temp5Vector2.y, 0);
-            Vector3 D6 = new Vector3(temp6Vector2.x, temp6Vector2.y, 0);
-            Vector3 D7 = new Vector3(temp7Vector2.x, temp7Vector2.y, 0);
-            Vector3 D8 = new Vector3(temp8Vector2.x, temp8Vector2.y, 0);
-            Vector3 D9 = new Vector3(temp9Vector2.x, temp9Vector2.y, 0);
-            Vector3 D10 = new Vector3(temp10Vector2.x, temp10Vector2.y, 0);
-            Vector3 D11 = new Vector3(temp11Vector2.x, temp11Vector2.y, 0);
-            Vector3 D12 = new Vector3(temp12Vector2.x, temp12Vector2.y, 0);
-            Vector3 D13 = new Vector3(temp13Vector2.x, temp13Vector2.y, 0);
-            Vector3 D14 = new Vector3(temp14Vector2.x, temp14Vector2.y, 0);
+            Vector3 R1 = new Vector3(temp1Vector2.x, temp1Vector2.y, 0);
+            Vector3 R2 = new Vector3(temp2Vector2.x, temp2Vector2.y, 0);
+            Vector3 R3 = new Vector3(temp3Vector2.x, temp3Vector2.y, 0);
+            Vector3 R4 = new Vector3(temp4Vector2.x, temp4Vector2.y, 0);
+            Vector3 R5 = new Vector3(temp5Vector2.x, temp5Vector2.y, 0);
+            Vector3 R6 = new Vector3(temp6Vector2.x, temp6Vector2.y, 0);
+            Vector3 R7 = new Vector3(temp7Vector2.x, temp7Vector2.y, 0);
+            Vector3 R8 = new Vector3(temp8Vector2.x, temp8Vector2.y, 0);
+            Vector3 R9 = new Vector3(temp9Vector2.x, temp9Vector2.y, 0);
+            Vector3 R10 = new Vector3(temp10Vector2.x, temp10Vector2.y, 0);
+            Vector3 R11 = new Vector3(temp11Vector2.x, temp11Vector2.y, 0);
+            Vector3 R12 = new Vector3(temp12Vector2.x, temp12Vector2.y, 0);
+            Vector3 R13 = new Vector3(temp13Vector2.x, temp13Vector2.y, 0);
+            Vector3 R14 = new Vector3(temp14Vector2.x, temp14Vector2.y, 0);
             Vector3 L1 = new Vector3(temp21Vector2.x, temp21Vector2.y, 0);
             Vector3 L2 = new Vector3(temp22Vector2.x, temp22Vector2.y, 0);
             Vector3 L3 = new Vector3(temp23Vector2.x, temp23Vector2.y, 0);
@@ -111,20 +111,20 @@ namespace SpaceshipGenerator
             Vector3 L13 = new Vector3(temp213Vector2.x, temp213Vector2.y, 0);
             Vector3 L14 = new Vector3(temp214Vector2.x, temp214Vector2.y, 0);
 
-            Vector3 D1B = new Vector3(temp1Vector2.x, temp1Vector2.y, 0.2f);
-            Vector3 D2B = new Vector3(temp2Vector2.x, temp2Vector2.y, 0.2f);
-            Vector3 D3B = new Vector3(temp3Vector2.x, temp3Vector2.y, 0.2f);
-            Vector3 D4B = new Vector3(temp4Vector2.x, temp4Vector2.y, 0.2f);
-            Vector3 D5B = new Vector3(temp5Vector2.x, temp5Vector2.y, 0.2f);
-            Vector3 D6B = new Vector3(temp6Vector2.x, temp6Vector2.y, 0.2f);
-            Vector3 D7B = new Vector3(temp7Vector2.x, temp7Vector2.y, 0.2f);
-            Vector3 D8B = new Vector3(temp8Vector2.x, temp8Vector2.y, 0.2f);
-            Vector3 D9B = new Vector3(temp9Vector2.x, temp9Vector2.y, 0.2f);
-            Vector3 D10B = new Vector3(temp10Vector2.x, temp10Vector2.y, 0.2f);
-            Vector3 D11B = new Vector3(temp11Vector2.x, temp11Vector2.y, 0.2f);
-            Vector3 D12B = new Vector3(temp12Vector2.x, temp12Vector2.y, 0.2f);
-            Vector3 D13B = new Vector3(temp13Vector2.x, temp13Vector2.y, 0.2f);
-            Vector3 D14B = new Vector3(temp14Vector2.x, temp14Vector2.y, 0.2f);
+            Vector3 R1B = new Vector3(temp1Vector2.x, temp1Vector2.y, 0.2f);
+            Vector3 R2B = new Vector3(temp2Vector2.x, temp2Vector2.y, 0.2f);
+            Vector3 R3B = new Vector3(temp3Vector2.x, temp3Vector2.y, 0.2f);
+            Vector3 R4B = new Vector3(temp4Vector2.x, temp4Vector2.y, 0.2f);
+            Vector3 R5B = new Vector3(temp5Vector2.x, temp5Vector2.y, 0.2f);
+            Vector3 R6B = new Vector3(temp6Vector2.x, temp6Vector2.y, 0.2f);
+            Vector3 R7B = new Vector3(temp7Vector2.x, temp7Vector2.y, 0.2f);
+            Vector3 R8B = new Vector3(temp8Vector2.x, temp8Vector2.y, 0.2f);
+            Vector3 R9B = new Vector3(temp9Vector2.x, temp9Vector2.y, 0.2f);
+            Vector3 R10B = new Vector3(temp10Vector2.x, temp10Vector2.y, 0.2f);
+            Vector3 R11B = new Vector3(temp11Vector2.x, temp11Vector2.y, 0.2f);
+            Vector3 R12B = new Vector3(temp12Vector2.x, temp12Vector2.y, 0.2f);
+            Vector3 R13B = new Vector3(temp13Vector2.x, temp13Vector2.y, 0.2f);
+            Vector3 R14B = new Vector3(temp14Vector2.x, temp14Vector2.y, 0.2f);
             Vector3 L1B = new Vector3(temp21Vector2.x, temp21Vector2.y, 0.2f);
             Vector3 L2B = new Vector3(temp22Vector2.x, temp22Vector2.y, 0.2f);
             Vector3 L3B = new Vector3(temp23Vector2.x, temp23Vector2.y, 0.2f);
@@ -144,96 +144,105 @@ namespace SpaceshipGenerator
             var points = new List<Vector3>(100)
             {
                 Z, A, B, C,
-                D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14,
+                //4
+                R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14,
                 //18
                 L1, L2, L3, L4, L5, L6, L7, L8, L9, L10, L11, L12, L13, L14,
-                //33
+                //32
                 E,
-
+                //33
                 ZB, AB, BB, CB,
-                //38
-                D1B, D2B, D3B, D4B, D5B, D6B, D7B, D8B, D9B, D10B, D11B, D12B, D13B, D14B,
-                //53
+                //37
+                R1B, R2B, R3B, R4B, R5B, R6B, R7B, R8B, R9B, R10B, R11B, R12B, R13B, R14B,
+                //51
                 L1B, L2B, L3B, L4B, L5B, L6B, L7B, L8B, L9B, L10B, L11B, L12B, L13B, L14B,
-                //68
+                //65
                 EB
             };
 
             var triangles = new[]
             {
-                //up 
-                3, 1, 2,
-                4, 3, 0,
-                5, 4, 0,
-                6, 5, 0,
-                7, 6, 0,
-                8, 7, 0,
-                9, 8, 0,
-                10, 9, 0,
-                11, 10, 0,
-                12, 11, 0,
-                13, 12, 0,
-                14, 13, 0,
-                15, 14, 0,
-                16, 15, 0,
-                17, 16, 0,
-
+                // up 
+                 3, 1, 2,
+                 4, 3, 0,
+                 5, 4, 0,
+                 6, 5, 0,
+                 7, 6, 0,
+                 8, 7, 0,
+                 9, 8, 0,
+                 10, 9, 0,
+                 11, 10, 0,
+                 12, 11, 0,
+                 13, 12, 0,
+                 14, 13, 0,
+                 15, 14, 0,
+                 16, 15, 0,
+                 17, 16, 0,
                 
-                // //14
-                33,41,40,
-                33,42,41,
-                33,43,42,
-                33,44,43,
-                33,45,44,
-                33,46,45,
                 
-                33,47,46,
-                33,48,47,
-                33,49,48,
-                33,50,49,
-                33,51,50,
-                33,52,51,
-                33,53,52,
-                33,55,54,
+                 0, 17, 18,
+                 0, 18, 19,
+                 0, 19, 20,
+                 0, 20, 21,
+                 0, 21, 22,
+                 0, 22, 23,
+                 0, 23, 24,
+                 0, 24, 25,
+                 0, 25, 26,
+                 0, 26, 27,
+                 0, 27, 28,
+                 0, 28, 29,
+                 0, 29, 30,
+                 0, 30, 31,
+                 
+                 0, 3, 18,
+                 3, 32, 1,
+                 
+                // Down
+                 //left side
+                 36, 35, 34,
+                 37, 33, 36,
+                 38, 33, 37,
+                 39, 33, 38,
+                 40, 33, 39,
+                 41, 33, 40,
+                 42, 33, 41,
+                 43, 33, 42,
+                 44, 33, 43,
+                 45, 33, 44,
+                 46, 33, 45,
+                 47, 33, 46,
+                 48, 33, 47,
+                 49, 33, 48,
+                 50, 33, 49,
+
+                 //right side
+                36, 34, 65,
+                51,36,33,
                 
-
-
-
-                0, 17, 18,
-                0, 18, 19,
-                0, 19, 20,
-                0, 20, 21,
-                0, 21, 22,
-                0, 22, 23,
-                0, 23, 24,
-                0, 24, 25,
-                0, 25, 26,
-                0, 26, 27,
-                0, 27, 28,
-                0, 28, 29,
-                0, 29, 30,
-                0, 30, 31,
-                0, 3, 18,
-                3, 32, 1,
+                52, 51, 33,
+                53, 52, 33,
+                54, 53, 33,
+                55, 54, 33,
+                56, 55, 33,
+                57, 56, 33,
+                58, 57, 33,
+                59, 58, 33,
+                60, 59, 33,
+                61, 60, 33,
+                62, 61, 33,
+                63, 62, 33,
+                64, 63, 33,
                 
-                //Down
-                36, 35, 34,
-                37, 33, 36,
-                38, 33, 37,
-                39, 33, 38,
-                40, 33, 39,
-                41, 33, 40,
-                42, 33, 41,
-                43, 33, 42,
-                44, 33, 43,
-                45, 33, 44,
-                46, 33, 45,
-                47, 33, 46,
-                48, 33, 47,
-                49, 33, 48,
-                50, 33, 49,
-                //ADD 33 AND REPLACE TGE TWO LAST 
             };
+            
+            
+            
+            
+            
+            
+            
+            
             // var topHalfSize = topSizeRange.RandomInside() / 2;
             // var sidesHalfSize = sidesSizeRange.RandomInside() / 2;
             //
